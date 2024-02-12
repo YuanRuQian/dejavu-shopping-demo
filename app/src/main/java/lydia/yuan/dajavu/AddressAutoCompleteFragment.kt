98b1935245f19d0bf9c558af06b1d610ad89c1a3
Lydia Yuan <lydiayuan99@gmail.com>
2024-02-12 09:56:13 -0500

    add: auto complete address

--- app/src/main/java/lydia/yuan/dajavu/AddressAutoCompleteFragment.kt
+++ app/src/main/java/lydia/yuan/dajavu/AddressAutoCompleteFragment.kt
@@ -1,24 +1,24 @@
 package lydia.yuan.dajavu
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import lydia.yuan.dajavu.databinding.FragmentAddressAutoCompleteBinding
 
-/**
- * An example full-screen fragment that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 class AddressAutoCompleteFragment : Fragment() {
 
 
-    private var dummyButton: Button? = null
-    private var fullscreenContent: View? = null
-    private var fullscreenContentControls: View? = null
-
+    private var autoCompleteFragment: AutocompleteSupportFragment? = null
     private var _binding: FragmentAddressAutoCompleteBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,6 +32,23 @@ class AddressAutoCompleteFragment : Fragment() {
     ): View? {
 
         _binding = FragmentAddressAutoCompleteBinding.inflate(inflater, container, false)
+
+        autoCompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+
+        autoCompleteFragment?.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        autoCompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
+
         return binding.root
 
     }
